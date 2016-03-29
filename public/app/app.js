fcfc0ce40f1dc0e0f0bee5ee3bf34f64622a8487
jeefer <retinasiri@gmail.com>
2016-03-29 16:57:28 +0100

    root

---
+++ public/app/app.js
@@ -0,0 +1,6 @@
+angular.module('app', []);
+
+angular.module('app').controller('testCtrl', function ($scope) {
+    $scope.jobs = [{title: 'Sales Person', description: 'you will flight dragons'},
+        {title: 'Accountant', description: 'you will use the keyboard'}];
+});
