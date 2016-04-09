be0e273dd47a4ee9f71b05f9a0ca7d335b578bed
jeefer <retinasiri@gmail.com>
2016-04-09 12:15:20 +0100

    added remote mongodb

--- server.js
+++ server.js
@@ -3,14 +3,37 @@
  */
 
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModels = require('./models/Job');
 
 var app = express();
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res){
+
+app.get('/api/jobs', function(req, res){
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    });
+});
+
+app.get('*', function(req, res){
     res.render('index');
 });
 
-app.listen(3000);
+//app.get('/', function(req, res){
+//    res.render('index');
+//});
+
+mongoose.connect("mongodb://dev:devops123@93.188.161.44:27017/test");
+
+var con = mongoose.connection;
+
+con.once('open', function(){
+   console.log('connect to mongodb successfully!');
+    jobModels.seedJobs();
+});
+
+
+app.listen(process.env.PORT, process.env.IP);
